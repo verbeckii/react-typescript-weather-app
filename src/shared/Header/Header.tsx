6679e89e1511304697b7c5286eebc5083176e6a3
Dmitrii Verbetsky <dmitrii@openocean.academy>
2021-10-26 23:36:46 +0300

    added font, styled react-select

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -14,6 +14,18 @@ export const Header = (props: Props) => {
         { value: 'vanilla', label: 'Vanilla' }
     ]
 
+    const colourStyles = {
+        control: (styles: any) => ({
+          ...styles,
+          backgroundColor: 'rgba(71, 147, 255, 0.2)',
+          width: '194px',
+          height: '37px',
+          border: 'none',
+          borderRadius: '10px',
+          zIndex: 100,
+        })
+    };
+
     return (
         <header className={s.header}>
             <div className={s.wrapper}>
@@ -25,8 +37,12 @@ export const Header = (props: Props) => {
             <div className={s.wrapper}>
                 <div className={s.change_theme}>
                     <GlobalSvgSelector id="change-theme" />
-                    <Select options={options} />
                 </div>
+                <Select 
+                    options={options} 
+                    styles={colourStyles}
+                    defaultValue={options[0]}
+                />
             </div>
         </header>
     )
