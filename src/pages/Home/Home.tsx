a9169fd84549fc4961a6d3e05c22d33eb0dc3b6a
Dmitrii Verbetsky <dmitrii@openocean.academy>
2021-10-28 23:55:59 +0300

    added ThisDayInfo component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { ThisDay } from './components/ThisDay/ThisDay'
+import { ThisDay } from './components/ThisDay/ThisDay';
+import { ThisDayInfo } from './components/ThisDayInfo/ThisDayInfo';
 
 import s from './Home.module.scss'
 
@@ -10,7 +11,10 @@ interface Props {
 export const Home = (props: Props) => {
     return (
         <div className={s.home}>
-            <ThisDay />
+            <div className={s.wrapper}>
+                <ThisDay />
+                <ThisDayInfo />
+            </div>
         </div>
     )
 }
