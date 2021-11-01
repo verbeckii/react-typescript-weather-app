6f54a758c9a7545cc58badabe776735856955a30
Dmitrii Verbetsky <dmitrii@openocean.academy>
2021-11-01 22:32:48 +0200

    added Popup

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { Route, Switch } from 'react-router'
 import { Home } from './pages/Home/Home';
 import { MonthStatistics } from './pages/MonthStatistics/MonthStatistics';
 import { Header } from './shared/Header/Header';
+import { Popup } from './shared/Popup/Popup';
 
 function App() {
   return (
-    <div className="container">
-      <Header/>
-      <Switch>
-        <Route path="/" 
-          exact component={Home}/>
-        <Route path="/month-statistics" 
-          exact component={MonthStatistics}/>
-      </Switch>
+    <div className="global-container">
+      {/* <Popup /> */}
+      <div className="container">
+        <Header/>
+        <Switch>
+          <Route path="/" 
+            exact component={Home}/>
+          <Route path="/month-statistics" 
+            exact component={MonthStatistics}/>
+        </Switch>
+      </div>
     </div>
   );
 }
